fe30501f0c0d1659bb34c28cc07520c48b16592d
unknown <madhavip42@gmail.com>
2024-07-26 14:27:48 +0100

    add project files

---
+++ Logics/server.js
@@ -0,0 +1,8 @@
+const express= require("express");
+const app= express();
+const session= require("express-session");
+
+
+app.listen(3000,()=>{
+    console.log("server is listening to 3000");
+})
--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data=initData.data.map((obj)=>({...obj,owner:"66870ba525e2b679d60de02f"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
